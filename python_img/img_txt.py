0e01381e1ee38425a4f730c5d74460668aca26ed
daniela2001-png <dmbmora@outlook.com>
2020-07-28 18:15:28 -0500

    submit changes completed

--- python_img/img_txt.py
+++ python_img/img_txt.py
@@ -3,76 +3,47 @@ import pytesseract
 import sys 
 from pdf2image import convert_from_path 
 import os 
-
+import re 
 
 
 PDF_file = "filename.pdf"
 
-# Store all the pages of the PDF in a variable 
+# Almacenar todas las páginas del PDF en una variable
 pages = convert_from_path(PDF_file, 500) 
   
-# Counter to store images of each page of PDF to image 
+# contador para el numero de paginas 
 image_counter = 1
   
-# Iterate through all the pages stored above 
+# loop que itera sobre el total de paginas 
 for page in pages: 
   
-    # Declaring filename for each page of PDF as JPG 
-    # For each page, filename will be: 
-    # PDF page 1 -> page_1.jpg 
-    # PDF page 2 -> page_2.jpg 
-    # PDF page 3 -> page_3.jpg 
-    # .... 
-    # PDF page n -> page_n.jpg 
     filename = "page_"+str(image_counter)+".jpg"
-      
-    # Save the image of the page in system 
+       
     page.save(filename, 'JPEG') 
-  
-    # Increment the counter to update filename 
+   
     image_counter = image_counter + 1
 
-''' 
-Part #2 - Recognizing text from the images using OCR 
-'''
 
-# Variable to get count of total number of pages 
+# var que obtiene el total de o numero de imgs 
 filelimit = image_counter-1
   
-# Creating a text file to write the output 
+# output .txt 
 outfile = "out_text.txt"
   
-# Open the file in append mode so that  
-# All contents of all images are added to the same file 
 f = open(outfile, "a") 
   
-# Iterate from 1 to total number of pages 
+# iteramos sobre el total de num pages 
 for i in range(1, filelimit + 1): 
-  
-    # Set filename to recognize text from 
-    # Again, these files will be: 
-    # page_1.jpg 
-    # page_2.jpg 
-    # .... 
-    # page_n.jpg 
+    #se genera los jpgs
     filename = "page_"+str(i)+".jpg"
           
-    # Recognize the text as string in image using pytesserct 
+    # usamos pytesserct 
     text = str(((pytesseract.image_to_string(Image.open(filename))))) 
   
-    # The recognized text is stored in variable text 
-    # Any string processing may be applied on text 
-    # Here, basic formatting has been done: 
-    # In many PDFs, at line ending, if a word can't 
-    # be written fully, a 'hyphen' is added. 
-    # The rest of the word is written in the next line 
-    # Eg: This is a sample text this word here GeeksF- 
-    # orGeeks is half on first line, remaining on next. 
-    # To remove this, we replace every '-\n' to ''. 
     text = text.replace('-\n', '')     
   
-    # Finally, write the processed text to the file. 
+    # escribimos en el file 
     f.write(text) 
   
-# Close the file after writing all the text. 
+# cerramos el archivo 
 f.close()
